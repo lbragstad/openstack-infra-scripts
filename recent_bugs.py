c41e3cd3eb40c2a62bc04e05fb73754b736114c7
Lance Bragstad <lbragstad@gmail.com>
2014-06-29 23:28:00 -0500

    Add recent_bugs.py script
    This script is similar to the infra_bugday.py script but reports the
    latest bugs in a Launchpad project.

---
+++ recent_bugs.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# brangler.py is intended to summarize bug information from Launchpad
+
+import argparse
+import datetime
+import os
+
+from launchpadlib.launchpad import Launchpad
+
+LPCACHEDIR = os.path.expanduser(os.environ.get('LPCACHEDIR',
+                                               '~/.launchpadlib/cache'))
+
+LPSTATUS = ('New', 'Confirmed', 'Triaged', 'In Progress')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='summarize bugs from a '
+             'launchpad project')
+    parser.add_argument('-p', '--project',
+            default='keystone',
+            help='launchpad project to pull bugs from')
+    args = parser.parse_args()
+
+    launchpad = Launchpad.login_anonymously('OpenStack Infra Bugday',
+                                            'production',
+                                            args.project)
+
+    project = launchpad.projects[args.project]
+    bug_counter = 0
+
+    for bug in project.searchTasks(status=LPSTATUS,
+                                   omit_duplicates=True,
+                                   order_by='-importance'):
+
+        invalid_states = ["Incomplete", "Opinion", "Won't Fix", "Expired",
+                          "Fix Released", "Fix Committed"]
+
+        if bug.status not in invalid_states:
+            # Create timedelta object to compare bug creation times against.
+            # Let's print out the bug from the last two days.
+            delta = datetime.timedelta(days=2)
+            today = datetime.datetime.today()
+            # Replace the tzinfo so it doesn't screw with comparisons
+            bug_created_time = bug.date_created.replace(tzinfo=None)
+            if bug_created_time >= today - delta:
+                print "%d. %s %s" % (bug_counter,
+                                     bug.importance,
+                                     bug.title)
+                print "%s" % (bug.web_link)
+                bug_counter += 1
+
+
+if __name__ == '__main__':
+    main()
